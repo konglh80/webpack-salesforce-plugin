1debae60cf474be490ca6ce0b274cf4aadc62504
Klaus Kong <klaus.kong@meginfo.com>
2018-08-02 16:05:13 +0800

    fix(): cannot open images after upload to Salesforce

--- src/index.js
+++ src/index.js
@@ -1,112 +1,149 @@
 import fs from 'fs';
-
 import objectAssign from 'object-assign';
-import {Connection} from 'jsforce';
+import { Connection } from 'jsforce';
 import glob from 'glob';
-import {union} from 'lodash';
+import { union } from 'lodash';
 import Zip from 'node-zip';
 
+const defaultOptions = {
+  salesforce: {
+    username: null,
+    password: null,
+    token: '',
+    loginUrl: 'https://login.salesforce.com'
+  },
+  resources: []
+};
+
 class WebpackSalesforcePlugin {
-    constructor(options = {}) {
-        let defaultOptions = {
-            salesforce: {
-                username: null,
-                password: null,
-                token: '',
-                loginUrl: 'https://login.salesforce.com'
-            },
-            resources: []
-        };
+  constructor(options = {}) {
+    this.options = objectAssign({}, options);
+    this.options.salesforce = objectAssign({}, defaultOptions.salesforce, options.salesforce);
+    this.options.resources = options.resources || defaultOptions.resources;
 
-        let mergedOptions = objectAssign({}, options);
-        mergedOptions.salesforce = objectAssign({}, defaultOptions.salesforce, options.salesforce);
-        mergedOptions.resources = options.resources || defaultOptions.resources;
-
-        this.options = mergedOptions;
-
-        if (!this.options.salesforce.username)
-            throw new Error('salesforce.username is required.');
-        if (!this.options.salesforce.password)
-            throw new Error('salesforce.password is required.');
-
-        this.options.resources.forEach((resource) => {
-            if (!resource.name)
-                throw new Error('Resource name is required.');
-        });
-
-        this.conn = new Connection({loginUrl: this.options.salesforce.loginUrl});
+    if (!this.options.salesforce.username) {
+      throw new Error('salesforce.username is required.');
     }
 
-    apply(compiler) {
-        compiler.plugin('after-emit', this.uploadFiles.bind(this))
+    if (!this.options.salesforce.password) {
+      throw new Error('salesforce.password is required.');
     }
 
-    uploadFiles(compilation, done) {
-        let resources = this.__globFiles().map((resource) => this.__zipResource(resource));
-        this.__doUpload(resources, done);
-    }
+    this.options.resources.forEach((resource) => {
+      if (!resource.name) {
+        throw new Error('Resource name is required.');
+      }
+    });
 
-    __doUpload(resources, done) {
-        console.log('Logging in to Salesforce.');
+    this.conn = new Connection({ loginUrl: this.options.salesforce.loginUrl });
+  }
 
-        this.conn.login(this.options.salesforce.username, this.options.salesforce.password + (this.options.salesforce.token || ''), (err, res) => {
-            if (err) {
-                done(err);
-            }
-            else {
-                console.log('Connected to Salesforce. Uploading resources.');
-                this.conn.metadata.upsert('StaticResource', resources, (err, results) => {
-                    if (err)
-                        done(err);
-                    else {
-                        if (results && results.length) {
-                            results.filter((r) => !r.success).forEach((r) => console.error(r));
-                        }
+  apply(compiler) {
+    compiler.plugin('after-emit', async (compilation, done) => {
+      const resources = this.__globFiles().map((resource) => this.__zipResource(resource));
+      try {
+        await this.login();
+        await this.upload(resources);
+        console.log('Upload complated!');
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  }
 
-                        done();
-                    }
-                });
-            }
-        });
-    }
+  login() {
+    console.log('Logging in to Salesforce.');
+    const promise = new Promise((resolve, reject) => {
+      const { username, password, token = '' } = this.options.salesforce;
+      this.conn.login(username, `${password}${token}`, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log('Connected to Salesforce.');
+          resolve(true);
+        }
+      });
+    });
 
-    __zipResource(globbedResource) {
-        if (globbedResource.files.length === 0) {
-            throw new Error('Resource ' + globbedResource.name + ' matched no files.');
+    return promise;
+  }
+
+  upload(resources) {
+    console.log('Uploading resources.');
+    const promise = new Promise((resolve, reject) => {
+      this.conn.metadata.upsert('StaticResource', resources, (err, results) => {
+        if (err) {
+          reject(err);
+
+          return;
         }
 
-        let zip = new Zip();
-        globbedResource.files.forEach((f) => {
-            if(f.match(/.*?woff(2?)$/)){
-                let data = fs.readFileSync(f);
-                zip.file(f, data);
-            } else {
-                let data = fs.readFileSync(f, 'utf8');
-                zip.file(f, data);
-            }
-        });
+        if (!results) {
+          reject(new Error('Upload resources failed. (No results)'));
+        } else if (toString.apply(results) === '[object Object]') {
+          if (results.success) {
+            resolve(true);
+          } else {
+            reject(results);
+          }
+        } else if (toString.apply(results) === '[object Array]') {
+          const errors = results.filter((r) => !r.success);
+          errors.forEach((r) => console.error(r))
+          if (errors.length > 0) {
+            reject(new Error('Upload resources failed. (With errors)'));
+          }
 
-        return {
-            fullName: globbedResource.name,
-            content: zip.generate({ base64: true, compression: 'DEFLATE' }),
-            contentType: 'application/zip'
-        };
+          resolve(true);
+        }
+      });
+    });
+
+    return promise;
+  }
+
+  __zipResource(globbedResource) {
+    if (globbedResource.files.length === 0) {
+      throw new Error('Resource ' + globbedResource.name + ' matched no files.');
     }
 
-    __globFiles() {
-        return this.options.resources.map((resource) => {
-            let filesToZip = resource.files.map((fileGlob) => {
-                return glob.sync(fileGlob, {});
-            }).reduce((currentList, nextList) => {
-                return union(currentList, nextList);
-            }, []);
+    const zip = new Zip();
+    globbedResource.files.forEach((f) => {
+      if (f.match(/.*?woff(2?)$/)) {
+        const data = fs.readFileSync(f);
+        zip.file(f, data);
+      } else if (f.match(/(png|jpg|jpeg|gif)$/)) {
+        const data = fs.readFileSync(f);
+        zip.file(f, data);
+      } else {
+        const data = fs.readFileSync(f, 'utf8');
+        zip.file(f, data);
+      }
+    });
 
-            return {
-                name: resource.name,
-                files: filesToZip
-            }
-        });
-    }
+    return {
+      fullName: globbedResource.name,
+      content: zip.generate({ base64: true, compression: 'DEFLATE' }),
+      contentType: 'application/zip'
+    };
+  }
+
+  __globFiles() {
+    return this.options.resources.map((resource) => {
+      const filesToZip = resource.files.map((fileGlob) => {
+        return glob.sync(fileGlob, {});
+      }).reduce((currentList, nextList) => {
+        return union(currentList, nextList);
+      }, []);
+
+      const result = {
+        name: resource.name,
+        files: filesToZip
+      };
+
+      return result;
+    });
+  }
 }
 
 module.exports = WebpackSalesforcePlugin;
